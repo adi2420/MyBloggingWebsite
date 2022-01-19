aa2e747390b3e3caf894a0abda37ee54795229f9
Aditya Bharadwaj <bhardwajaditya31@gmail.com>
2022-01-19 10:58:13 +0530

    user login page with post request

--- index.js
+++ index.js
@@ -9,6 +9,15 @@ const path = require('path');
 
 
 const Post = require('./database/models/Post');
+const storePost=require("./middleware/storePost");
+const createPostController = require('./controllers/createPost')
+const homePageController = require('./controllers/homePage')
+const storePostController = require('./controllers/storePost')
+const getPostController = require('./controllers/getPost')
+const createUserController = require("./controllers/createUser");
+const storeUserController = require('./controllers/storeUser');
+const loginController = require("./controllers/login");
+const loginUserController = require('./controllers/loginUser');
 
 app.use(express.static('public'));
 config({ cache: process.env.NODE_ENV === 'production' });
@@ -22,6 +31,7 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({
     extended: true
 }));
+app.use('/posts/store', storePost);
 
 mongoose.connect('mongodb://localhost:27017/node-blog', { useNewUrlParser: true })
     .then(() => 'You are now connected to Mongo!')
@@ -30,23 +40,23 @@ mongoose.connect('mongodb://localhost:27017/node-blog', { useNewUrlParser: true 
 // app.get('/', (req, res) => {
 //     res.sendFile(path.resolve(__dirname, 'pages/index.html'));
 // });
-app.get('/', async (req, res) => {
-    const posts = await Post.find({})
-    res.render('index', {
-        posts
-    })
-});
+// app.get('/', async (req, res) => {
+//     const posts = await Post.find({})
+//     res.render('index', {
+//         posts
+//     })
+// });
 
-app.get('/posts/new',(req,res)=>{
-    res.render('create');
-});
+// app.get('/posts/new',(req,res)=>{
+//     res.render('create');
+// });
 
-app.get('/post/:id', async (req, res) => {
-    const post = await Post.findById(req.params.id)
-    res.render('post', {
-        post
-    });
-});
+// app.get('/post/:id', async (req, res) => {
+//     const post = await Post.findById(req.params.id)
+//     res.render('post', {
+//         post
+//     });
+// });
 
 
 //--------static pages-------------------------------------
@@ -69,20 +79,30 @@ app.get('/contact.html', (req, res) => {
 //-------------------post --------------------
 
 
-app.post("/posts/store", (req, res) => {
-    const {
-        image
-    } = req.files
+// app.post("/posts/store", (req, res) => {
+//     const {
+//         image
+//     } = req.files
 
-    image.mv(path.resolve(__dirname, 'public/posts', image.name), (error) => {
-        Post.create({
-            ...req.body,
-            image: `/posts/${image.name}`
-        }, (error, post) => {
-            res.redirect("/");
-        });
-    });
-});
+//     image.mv(path.resolve(__dirname, 'public/posts', image.name), (error) => {
+//         Post.create({
+//             ...req.body,
+//             image: `/posts/${image.name}`
+//         }, (error, post) => {
+//             res.redirect("/");
+//         });
+//     });
+// });
+
+app.get("/", homePageController);
+app.get("/post/:id", getPostController);
+app.get("/posts/new", createPostController);
+app.post("/posts/store", storePostController);
+app.get('/auth/login', loginController);
+app.post('/users/login', loginUserController);
+app.get("/auth/register", createUserController);
+app.post("/users/register", storeUserController);
+
 
 
 
