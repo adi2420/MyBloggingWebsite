7db7af4ed12f2ca6adf9ffb059de55c48cc07ee9
Aditya Bharadwaj <bhardwajaditya31@gmail.com>
2022-01-19 12:47:13 +0530

    Website is working with basic functionalities

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const mongoose=require("mongoose");
 const bodyParser=require("body-parser");
 const fileUpload=require("express-fileupload")
 const path = require('path');
+const expressSession=require("express-session");
+const connectMongo=require("connect-mongo");
+const connectFlash=require("connect-flash");
+const edge=require("edge.js");
 
 
 
@@ -18,11 +22,25 @@ const createUserController = require("./controllers/createUser");
 const storeUserController = require('./controllers/storeUser');
 const loginController = require("./controllers/login");
 const loginUserController = require('./controllers/loginUser');
+const auth=require('./middleware/auth');
+const redirectIfAuthenticated=require('./middleware/redirectIfAuthenticated');
+const logoutController=require('./controllers/logout');
 
 app.use(express.static('public'));
 config({ cache: process.env.NODE_ENV === 'production' });
 app.use(engine);
 app.use(fileUpload());
+app.use(connectFlash());
+
+
+const mongoStore = connectMongo(expressSession);
+
+app.use(expressSession({
+    secret: 'secret',
+    store: new mongoStore({
+        mongooseConnection: mongoose.connection
+    })
+}));
 
 
 app.set('views',__dirname+'/views');
@@ -32,6 +50,10 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 app.use('/posts/store', storePost);
+app.use('*', (req, res, next) => {
+    edge.global('auth', req.session.userId)
+    next()
+});
 
 mongoose.connect('mongodb://localhost:27017/node-blog', { useNewUrlParser: true })
     .then(() => 'You are now connected to Mongo!')
@@ -96,12 +118,14 @@ app.get('/contact.html', (req, res) => {
 
 app.get("/", homePageController);
 app.get("/post/:id", getPostController);
-app.get("/posts/new", createPostController);
-app.post("/posts/store", storePostController);
-app.get('/auth/login', loginController);
-app.post('/users/login', loginUserController);
-app.get("/auth/register", createUserController);
-app.post("/users/register", storeUserController);
+app.get("/posts/new", auth,createPostController);
+app.post("/posts/store", auth,storePost,storePostController);
+app.get("/auth/logout",  logoutController);
+app.get('/auth/login',redirectIfAuthenticated, loginController);
+app.post('/users/login',redirectIfAuthenticated ,loginUserController);
+app.get("/auth/register", redirectIfAuthenticated,createUserController);
+app.post("/users/register", redirectIfAuthenticated,storeUserController);
+
 
 
 
